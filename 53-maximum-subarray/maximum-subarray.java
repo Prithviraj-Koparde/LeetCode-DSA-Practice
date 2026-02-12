bd44204771e895c1c4f8e5baf75f61ffbfa91d7b
Prithviraj Koparde <157233644+Prithviraj-Koparde@users.noreply.github.com>
2026-02-12 12:21:50 +0530

    Time: 1 ms (99.62%) | Memory: 77.1 MB (91.60%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = Integer.MIN_VALUE; // or we can write it as maxSum = Integer.MAX_VALUE;
+        int maxSum = Integer.MIN_VALUE; // we cannot write maxSum = 0 it will give error for an edge case where array consist of only -1 element
         int currentSum = 0;
 
         for (int i = 0; i < nums.length; i++) {
